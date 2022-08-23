4628d16875a086d29f259f9b07b3126d6852de93
Fabian Lee <flee999@hotmail.com>
2022-08-23 13:04:09 -0400

    work on resource server methods

--- spring-security-oauth2-resource-server/src/main/java/org/fabianlee/springsecurityoauth2resource/user/User.java
+++ spring-security-oauth2-resource-server/src/main/java/org/fabianlee/springsecurityoauth2resource/user/User.java
@@ -1,6 +1,8 @@
 package org.fabianlee.springsecurityoauth2resource.user;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private String name;
 
     public String getName() {
--- spring-security-oauth2-resource-server/src/main/java/org/fabianlee/springsecurityoauth2resource/user/UserController.java
+++ spring-security-oauth2-resource-server/src/main/java/org/fabianlee/springsecurityoauth2resource/user/UserController.java
@@ -8,11 +8,14 @@ import java.util.Map;
 import org.fabianlee.springsecurityoauth2resource.oauth2.OAuth2Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,8 +32,8 @@ public class UserController {
 
         private List userListV1 = new ArrayList(Arrays.asList(new User("moe"), new User("larry"), new User("curly")));
         
-        @GetMapping(value="/test")
-        public String test() {
+        @GetMapping(value="/me")
+        public String me() {
         	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         	log.debug(auth.toString());
         	log.debug("auth authorities: " + auth.getAuthorities());
@@ -44,7 +47,13 @@ public class UserController {
         	for(Map.Entry<String,Object> entry : jwt.getToken().getClaims().entrySet() ) {
         		log.debug("claim: " + entry.getKey() + "=" + entry.getValue());
         	}
-        	return "test";
+        	
+        	StringBuffer sbuf = new StringBuffer();
+        	sbuf.append("email: " + jwt.getToken().getClaimAsString("email") + "\n");
+        	sbuf.append("authorities: " + auth.getAuthorities() + "\n");
+        	sbuf.append("group: " + jwt.getToken().getClaimAsString("group") + "\n");
+        	sbuf.append("scp: " + jwt.getToken().getClaimAsString("scp") + "\n");
+        	return sbuf.toString();
         }
 
         @GetMapping
@@ -57,27 +66,48 @@ public class UserController {
             return userListV1;
         }
         
-        @GetMapping(value="app1")
-        @PreAuthorize("hasAuthority('app1')")
-        public Iterable scope_app1() {
-            log.debug("app1");
-            return userListV1;
-        }
-        @GetMapping(value="app2")
-        @PreAuthorize("hasAuthority('SCOPE_api_get')")
-        public Iterable scope_app2() {
-            log.debug("app2");
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            log.debug("user email: " + ((JwtAuthenticationToken)authentication).getToken().getClaimAsString("email"));
-            return userListV1;
-        }
-        
         @DeleteMapping
-        @PreAuthorize("hasRole('managers')")
-        public void deleteUser() {
+        @PreAuthorize("hasRole('managers') && hasAuthority('SCOPE_api_delete')")
+        public Iterable deleteUser() {
+        	int nusers = userListV1.size();
             log.debug("called deleteUser");
             if (userListV1.size() > 0) {
                 userListV1.remove(userListV1.size() - 1);
             }
+        	log.debug("deleteUser: #users was " + nusers + " and is now  " + userListV1.size());
+
+            return userListV1;
         }
+        
+        @GetMapping(value="/engineer")
+        @PreAuthorize("hasRole('engineers') || hasRole('managers')")
+        public ResponseEntity<List<User>> listEngineers() {
+            log.debug("listEngineers");
+            
+            List<User> users = new ArrayList<User>(
+            		Arrays.asList(
+            				new User("engineer1"),
+            				new User("engineer2")
+            				) 
+            		);
+            return new ResponseEntity<List<User>>(users,HttpStatus.OK);
+        }
+        
+        @GetMapping(value="/manager")
+        @PreAuthorize("hasRole('managers')")
+        public ResponseEntity<List<User>> listManagers() {
+            log.debug("listManagers");
+            
+            // log user email to show how audit entry could be created for sensitive info
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            log.debug("AUDIT info requested by: " + ((JwtAuthenticationToken)authentication).getToken().getClaimAsString("email"));
+            
+            List<User> managers = new ArrayList<User>( 
+            		Arrays.asList(
+            				new User("manager1")
+            				) 
+            		);
+            return new ResponseEntity<List<User>>(managers,HttpStatus.OK);
+        }
+        
 }
