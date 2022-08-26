e7775314930c471f92ee5bba51f2acbed2a69746
Fabian Lee <flee999@hotmail.com>
2022-08-26 12:35:04 -0400

    docs

--- spring-security-oauth2-client-app/src/main/java/org/fabianlee/springsecurityoauth2client/oauth2/SecurityConfig.java
+++ spring-security-oauth2-client-app/src/main/java/org/fabianlee/springsecurityoauth2client/oauth2/SecurityConfig.java
@@ -1,7 +1,12 @@
 /**
- * For Spring 5.7.1 or greater, WebSecurityConfigurerAdapter is DEPRECATED !!!!
- * See the class SecurityConfiguration as replacement
+ * For Spring Security 5.7.1+ OR Spring Boot 2.7.0+, WebSecurityConfigurerAdapter is DEPRECATED !!!!
+ * Configure through component based security configuration instead, see my newer implementation here: 
+ * https://github.com/fabianlee/spring-boot-security5-oauth2-oidc/blob/main/spring-security5-oauth2-client-app/src/main/java/org/fabianlee/springsecurityoauth2client/oauth2/SecurityConfig.java
+ * 
+ * https://www.springcloud.io/post/2022-02/spring-security-deprecate-websecurityconfigureradapter
+ * https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
  */
+
 package org.fabianlee.springsecurityoauth2client.oauth2;
 
 import org.springframework.context.annotation.Configuration;
